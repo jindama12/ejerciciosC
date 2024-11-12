8db1850877dfcc8cbacf44d5a6b85f5ca0e3def7
jindama12 <hugo296cm@gmail.com>
2024-11-12 09:55:15 +0100

    updated

--- ejemplos/prexecl.c
+++ ejemplos/prexecl.c
@@ -4,6 +4,6 @@
 void main(){
 	printf("Ejemplo de uso de exec():\n");
 	printf("listado del directorio:");
-   	execl("/bin/ls", "ls", "-l", (char *)NULL);
+   	execl("/bin/ls", "ls", "-l", (char *)NULL); //ejecuta este comando y no se ejecuta el resto del programa --> (char *)NULL envia los errores a la basura
 	printf("\nFin del programa...\n");
 }
--- ejemplos/prsystem.c
+++ ejemplos/prsystem.c
@@ -4,7 +4,7 @@
 void main(){
 	printf("Ejemplo de uso de system():\n");
 	printf("\t listado del directorio actual y envío a un fichero:");
-	printf("%d",system("ls >salida.txt"));
+	printf("%d",system("ls >salida.txt")); //system crea un hijo que ejecuta ese comandos (el programa sigue)
 	printf("\n\tAbrimos el fichero con gedit...");
         printf("%d",system("gedit salida.txt"));
 	printf("\n\tEste comando es erróneo: %d",system("gegedit"));
